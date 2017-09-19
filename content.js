7597450d6b2b12eba7b72259c0d91ac58dc64c0b
Robert Greenberg <robert.greenberg@tufts.edu>
2017-09-19 16:10:33 +0100

    works rough

--- content.js
+++ content.js
@@ -1,3 +1,24 @@
 // content.js
 
-alert("Hello from your Chrome extension!")
+
+alert("Don't fucking do this!")
+alert("please dont fucking do this")
+
+var time = prompt("How many minutes do you want to take a study break for?")
+var min  = time * 60;
+setTimeout(myFunction, min);
+
+
+function myFunction() {
+    
+
+
+var sign = prompt("Are you sure this is worth your time?")
+if (sign.toLowerCase() == "yes") {
+  open("http://www.pmslweb.com/the-blog/wp-content/uploads/2015/02/27-stop-being-a-dumbass.jpg", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000")
+}
+
+open("http://s2.quickmeme.com/img/17/17f9852034eb0873259cd6eee79318fe62cf1262a9292e03e522c41c2e48fe6d.jpg", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000")
+
+}
+
