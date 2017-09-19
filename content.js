6c06ff3f3060b48f2a562a129427137055301308
Robert Greenberg <robert.greenberg@tufts.edu>
2017-09-19 16:55:25 +0100

    Working V1

--- content.js
+++ content.js
@@ -1,24 +1,67 @@
 // content.js
 
 
-alert("Don't fucking do this!")
-alert("please dont fucking do this")
+alert("You were doing so well, being so studious! Don't fucking do this!")
+alert("Please, I know you don't want to fucking get off topic.")
 
-var time = prompt("How many minutes do you want to take a study break for?")
-var min  = time * 60;
+var a = prompt("Are you fucking positive you need a study break right now? I think you are just being a lazy fuck.")
+if (a.toLowerCase() == "yes")
+{
+	var b = prompt("Wow, so you are pretty fucking serious about this, right?")
+	if (b.toLowerCase() == "yes")
+	{
+		var c = prompt("You're being a dumbass because you have real work to do, are you sure this is worth your time?")
+		if (c.toLowerCase() == "yes")
+		{
+
+			var d = prompt("I'll try one last time you fucking idiot, you really NEED a fucking break right now?")
+			if (d.toLowerCase() == "yes")
+			{
+
+			}
+			else
+			{
+				alert("That's right, MOTHA FUCKAAAAAAAH")
+				exit()
+			}
+
+		}
+		else
+		{
+			alert("Do your real work, ok?")
+			exit()
+		}
+	}
+	else
+	{
+		alert("Angry Rob is always right, now get back to work, bitch")
+		exit()
+	}
+
+}
+else
+{
+	alert("I thought so...now get back to work.")
+	exit()
+}
+
+
+var time = prompt("This is a mistake, but fine...it's your life, how many minutes do you want to take a study break for?")
+var min  = time * 60000;
 setTimeout(myFunction, min);
 
 
 function myFunction() {
-    
+  open("http://s2.quickmeme.com/img/a8/a882cc6dace90fd41f1ea6afba6b2fbfba18f230c23e9e8ec000789ec98ecacf.jpg", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000")
 
 
-var sign = prompt("Are you sure this is worth your time?")
+
+/*var sign = prompt("Are you sure this is worth your time?")
 if (sign.toLowerCase() == "yes") {
   open("http://www.pmslweb.com/the-blog/wp-content/uploads/2015/02/27-stop-being-a-dumbass.jpg", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000")
 }
 
 open("http://s2.quickmeme.com/img/17/17f9852034eb0873259cd6eee79318fe62cf1262a9292e03e522c41c2e48fe6d.jpg", "_blank","toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=4000,height=4000")
-
+*/
 }
 
